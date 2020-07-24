7b93ddf657f3384d02638a4c266c06c3382a313f
Tania Choudhury <hchoudhurytania@gmail.com>
2020-07-24 18:53:44 +0530

    "pushed"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { LayoutsModule } from './layouts/layouts.module';
 import { AppRoutingModule } from './app-routing.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { RatingComponent } from './rating/rating.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    RatingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LayoutsModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
